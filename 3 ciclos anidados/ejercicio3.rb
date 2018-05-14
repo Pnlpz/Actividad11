56f012367521393bb514052d41b269c3fd7093c9
Marisol Smith <marisolsmith@gmail.com>
2018-05-14 12:28:51 -0400

    listo

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -4,19 +4,20 @@
 # Ingrese un número (0 para salir): _
 
 print "Ingresa un numero"
-num = gets.chomp.to_i
+  #num = gets.chomp.to_i
+  # unless num == 0
+  #      mul = i * num
+  #      puts "#{num} * #{i} = #{mul}"
+  # end
+  # exit if num == 0
 
-unless num == 0 for i in (1..12)
-     mul = i * num
-     puts "#{num} * #{i} = #{mul}"
-end
-
-#no lo terminé. No entiendo cómo hacer que salga del programa.
+# no lo terminé. No entiendo cómo hacer que salga del programa.
 
 
 num = gets.chomp
 num = num.to_i
 for i in (1..12)
-     mul = i * num
-     puts "#{num}*#{i} = #{mul}"
+  mul = i * num
+  puts "#{num}*#{i} = #{mul}"
 end
+exit if num == 0
